7ac7702a8f140fe556354ad4bcbeddce28688fb7
Nastya07s <nastya.kotovich07@gmail.com>
2020-02-26 16:43:35 +0300

    feat: solve reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let result = String(n);
+  if(result[0]=='-'){
+    return result.split('').reverse().join('').slice(0, -1);
+  }
+  return result.split('').reverse().join('');
 }
